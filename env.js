9aab8ce99d98a92b63ee5fe567a55d2d54b6ea8a
KimSalahBim <kimruneholmvik@gmail.com>
2026-01-25 10:01:02 +0100

    Add files via upload

---
+++ env.js
@@ -0,0 +1,4 @@
+// env.js (PUBLIC) – brukes av GitHub Pages (ingen Vercel-ENV der)
+window.ENV = window.ENV || {};
+window.ENV.SUPABASE_URL = "https://jxteosjxgrblasksfeyu.supabase.co";
+window.ENV.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4dGVvc2p4Z3JibGFza3NmZXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxOTE1ODMsImV4cCI6MjA4NDc2NzU4M30.4VhLfxcX0PCkUeK9aYrTrfmTwESH9zOyx4sY61lIZ9w";
